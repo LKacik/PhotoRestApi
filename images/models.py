0b612d7af85badcd3cf4ae88cadfbca07abc6664
LKacik <cep6@poczta.fm>
2022-07-13 21:15:46 +0200

    Updated

--- images/models.py
+++ images/models.py
@@ -2,13 +2,15 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+# Create your models here.
+
 
 def user_directory_path(instance, filename):
-    return 'image/{0}'.format(filename)
+    return 'images/{0}'.format(filename)
 
 
 class Category(models.Model):
-    name = models.CharField(max_lenght=100)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
@@ -16,7 +18,7 @@ class Category(models.Model):
 
 class Images(models.Model):
 
-    option = (
+    options = (
         ('active', 'Active'),
         ('deactivated', 'Deactivated'),
     )
@@ -25,9 +27,10 @@ class Images(models.Model):
     title = models.CharField(max_length=250)
     alt = models.TextField(null=True)
     image = models.ImageField(
-        upload_to=user_directory_path, default='post/default.jpg')
+        upload_to=user_directory_path, default='posts/default.jpg')
     slug = models.SlugField(max_length=250, unique_for_date='created')
+    created = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(
         User, on_delete=models.PROTECT, related_name='author')
-    status = models.CharField(max_length=11, choices=option, default='active')
+    status = models.CharField(max_length=11, choices=options, default='active')
 
